78fbe9cd0d10ef88b463ee26ac4bc917bb43447a
lazyChan297 <lazyChan297@163.com>
2019-03-01 17:57:19 +0800

    0302

--- src/App.js
+++ src/App.js
@@ -1,28 +1,111 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { comment } from 'postcss';
 
-class App extends Component {
+// state 组件用于保存、控制、修改自己的可变状态，
+// 一定要通过setState修改才会触发试图的更新
+// 外部不能访问也不能修改
+
+// props 可以通过父组件传入配置参数
+// 外部传递进来的内部无法修改和控制，除非父组件重新渲染
+
+const users = [
+  { username: 'Jerry', age: 21, gender: 'male' },
+  { username: 'Tomy', age: 22, gender: 'male' },
+  { username: 'Lily', age: 19, gender: 'female' },
+  { username: 'Lucy', age: 20, gender: 'female' }
+]
+
+class Title extends Component {
+  handleClickOntitle() {
+    console.log(this)
+  }
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
+      // 如果想获得当前的实例,需要通过bind(this)
+      <h1 onClick={this.handleClickOntitle.bind(this)}>Title</h1>
+    )
   }
 }
 
-export default App;
+class User extends Component {
+  render() {
+    const {user} = this.props
+    return (
+      <div>
+        <div>name: {user.username}</div>
+      </div>
+    )
+  }
+}
+class Header extends Component {
+  render() {
+    return (
+      <div>
+        <Title/>
+        <h2>this is header</h2>
+      </div>
+    )
+  }
+}
+
+class Main extends Component {
+  render () {
+    return (
+      <div>
+        <h2>this is main content</h2>
+        <LikeButton/>
+        <LikeButton likedText="赞" unlikedText="已赞" />
+        <div>{users.map((user,i)=> <User key={i} user={user}/>)}</div>
+      </div>
+    )
+  }
+}
+
+class Footer extends Component {
+  render() {
+    return (
+      <div>
+        <h2>this is Footer</h2>
+      </div>
+    )
+  }
+}
+
+class LikeButton extends Component{
+  static defaultProps = {
+    likedText: '取消',
+    unlikedText: '点赞'
+  }
+  constructor() {
+    super()
+    this.state = {
+      isLiked: false
+    }
+  }
+  handleClickOnLikeButton() {
+    this.setState({
+      isLiked: !this.state.isLiked
+    })
+  }
+  render() {
+    return (
+      <button onClick={this.handleClickOnLikeButton.bind(this)}>
+        {this.state.isLiked?this.props.likedText:this.props.unlikedText}
+      </button>
+    )
+  }
+}
+
+class Index extends Component {
+  render() {
+    return (
+      <div>
+        <Header/>
+        <Main/>
+        <Footer/>
+      </div>
+    )
+  }
+}
+
+export default Index;
