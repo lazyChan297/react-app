073a8f153872ba2fd8a9b53ece94e5b5e985d066
lazyChan297 <lazyChan297@163.com>
2019-03-05 18:30:06 +0800

    add react-redux

---
+++ src/Content.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
+import ThemeSwitch from './ThemeSwitch'
+import {connect} from './react-redux'
+
+class Content extends Component {
+    static propTypes = {
+        themeColor: PropTypes.string
+    }
+    render() {
+        return (
+            <div>
+                <p style={{color:this.props.themeColor}}>react.js小书内容</p>
+                <ThemeSwitch></ThemeSwitch>
+            </div>
+        )
+    }
+}
+
+const mapStateToProps = (state) => {
+    return {
+      themeColor: state.themeColor
+    }
+}
+Content = connect(mapStateToProps)(Content)
+export default Content
